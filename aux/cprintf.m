576cddc6646409bf341e229cf17ce83a90ce4a6f
williameclee <williameclee@gmail.com>
2026-02-14 12:21:49 -0700

    Added error for second output

--- aux/cprintf.m
+++ aux/cprintf.m
@@ -1,7 +1,7 @@
 %% CPRINTF - FPRINTF wrapper removing HTML tags when appropriate
-% Should behave exactly like FPRINTF, but if HotLinks is disabled (i.e. in
-% a terminal environment), it will remove any <a></a> tags from the input
-% string before printing.
+% Should behave exactly like FPRINTF when logging to screen (won't work for
+% to files), but if HotLinks is disabled (i.e. in a terminal environment),
+% it will remove any <a></a> tags from the input string before printing.
 %
 % Author
 %   2026/02/14, williameclee@arizona.edu (@williameclee)
@@ -10,10 +10,10 @@ function varargout = cprintf(varargin)
     txt = sprintf(varargin{:});
 
     if feature('HotLinks')
-        cnt = fprintf('%s', txt);
+        [cnt, err] = fprintf('%s', txt);
 
         if nargout > 0
-            varargout = {cnt};
+            varargout = {cnt, err};
         else
             varargout = {};
         end
@@ -32,10 +32,10 @@ function varargout = cprintf(varargin)
     txt = regexprep(txt, '<a[^>]*>', '');
     txt = strrep(txt, '</a>', '');
 
-    cnt = fprintf('%s', txt);
+    [cnt, err] = fprintf('%s', txt);
 
     if nargout > 0
-        varargout = {cnt};
+        varargout = {cnt, err};
     else
         varargout = {};
     end
