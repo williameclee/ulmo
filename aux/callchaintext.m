ae827af91a920e74069bacb12fa16e4d64dfe2fc
williameclee <williameclee@gmail.com>
2026-02-14 15:45:18 -0700

    Squashed commit of the following:
    commit f0db776b802d407ef5c06c21746bbc37a93f5288
    Author: En-Chi Lee <williameclee@gmail.com>
    Date:   Sat Feb 14 15:42:43 2026 -0700
    
        Feature: CALLCHAINTEXT removes consecutive identical function calls (#26)
    
        * Initial commit
    
        * Added extra guard rails
    
        * Added tests
    
        * Made sure support for column vectors

--- aux/callchaintext.m
+++ aux/callchaintext.m
@@ -1,7 +1,27 @@
+%% CALLCHAINTEXT - Generates a call stack for a list of functions.
+% Syntax
+%   txt = callchaintext(fun)
+%   txt = callchaintext([fun1, fun2, ...])
+%
+% Input arguments
+%   fun - Function name as a character vector or string scalar
+%
+% Output arguments
+%   txt - Call stack as a character vector with clickable links to the
+%       functions
+%
+% Last modified
+%   2026/02/14, williameclee@arizona.edu (@williameclee)
+%      - Removed consecutive identical functions from the input list
+
 function callChainTxt = callchaintext(funs)
 
-    arguments
+    arguments (Input)
         funs {mustBeA(funs, {'char', 'string', 'cell'})}
+    end
+
+    arguments (Output)
+        callChainTxt char {mustBeTextScalar}
     end
 
     if iscell(funs)
@@ -14,7 +34,23 @@ function callChainTxt = callchaintext(funs)
         funs = {funs};
     end
 
-    callChainTxt = cellfun(@(x) sprintf('<a href="matlab: open(''%s'')">%s</a>', which(x), x), funs, "UniformOutput", false);
-    callChainTxt = strjoin(callChainTxt, '>');
+    funs = cellfun(@char, funs, "UniformOutput", false);
+    funs = funs(:)'; % Ensure funs is a row cell vector
+
+    % Handle empty input: return an empty string scalar for an empty call chain.
+    if all(cellfun(@isempty, funs))
+        callChainTxt = '';
+        return
+    end
+
+    % Remove consecutive identical elements
+    if length(funs) > 1
+        funs = funs([true, ~strcmp(funs(1:end - 1), funs(2:end))]);
+    end
+
+    callChainTxt = cellfun(@(x) ...
+        sprintf('<a href="matlab: open(''%s'')">%s</a>', which(x), x), funs, ...
+        "UniformOutput", false);
+    callChainTxt = char(strjoin(callChainTxt, '>'));
 
 end
